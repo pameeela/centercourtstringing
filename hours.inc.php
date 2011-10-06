7b5bb983bfb2097fafd9ac1ec1519e7a1e442856
Pamela <pnbarone@gmail.com>
2011-10-06 11:47:25 +1100

    "hours update"

--- hours.inc.php
+++ hours.inc.php
@@ -1,12 +1,12 @@
 <table summary="hours" class="hours">
-<tr><td colspan="2" align="center"><h3>Summer hours</h3></td></tr>
+<tr><td colspan="2" align="center"><h3>Shop hours</h3></td></tr>
 <tr>
-<td>Monday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>Tuesday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>&nbsp;&nbsp;Wednesday&nbsp;&nbsp;</td><td align="center">4 pm-8 pm</td></tr>
-<tr><td>Thursday</td><td align="center">Closed</td></tr>
-<tr><td>Friday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>Saturday</td><td align="center">Closed</td></tr>
+<td>Monday</td><td align="center">9 am-7 pm</td></tr>
+<tr><td>Tuesday</td><td align="center">9 am-7 pm</td></tr>
+<tr><td>&nbsp;&nbsp;Wednesday&nbsp;&nbsp;</td><td align="center">9 am-7 pm</td></tr>
+<tr><td>Thursday</td><td align="center">9 am-7 pm</td></tr>
+<tr><td>Friday</td><td align="center">9 am-5 pm</td></tr>
+<tr><td>Saturday</td><td align="center">8 am-3 pm</td></tr>
 <tr><td>Sunday</td><td align="center">Closed</td>
 </tr>
 </table>
--- rightcol.inc.php
+++ rightcol.inc.php
@@ -1,16 +1,5 @@
 <a href="/scheduleapickup"><img src="images/pickupbutton.gif" alt="Schedule a racquet pickup online" class="" /></a>
 <div id="hours">
-<table summary="hours" class="hours">
-<tr><td colspan="2" align="center"><h3>Summer hours</h3></td></tr>
-<tr>
-<td>Monday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>Tuesday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>&nbsp;&nbsp;Wednesday&nbsp;&nbsp;</td><td align="center">4 pm-8 pm</td></tr>
-<tr><td>Thursday</td><td align="center">Closed</td></tr>
-<tr><td>Friday</td><td align="center">10 am-2 pm</td></tr>
-<tr><td>Saturday</td><td align="center">Closed</td></tr>
-<tr><td>Sunday</td><td align="center">Closed</td>
-</tr>
-</table>
+<?php include("hours.inc.php"); ?>
 </div>
 <a href="http://centercourtstringing.com"><img src="images/center_court_166.jpg" alt="Center Court logo" /></a>
